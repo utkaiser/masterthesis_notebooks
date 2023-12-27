c925e383a5b0ee3eea0b6dbb0e2895016194988e
luiskaiser <luiskaiser@t-online.de>
2023-12-27 23:04:13 +0100

    fix generate_data

--- utils_generating_data.py
+++ utils_generating_data.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy.ndimage
+import torch
 from skimage.transform import resize
+
+from utils_use_numerical_solver import init_pulse_gaussian, pseudo_spectral_tensor, velocity_verlet_tensor
+from wave_component_function import WaveEnergyComponentField_tensor, WaveSol_from_EnergyComponent_tensor
 
 
 def generate_velocity_profile_crop(v_images, m, output_path, num_times):
@@ -49,3 +53,89 @@ def generate_velocity_profile_crop(v_images, m, output_path, num_times):
             wavespeed_list.append(resize(npimg, (m, m), order=4))
 
     np.savez(output_path, wavespeedlist=wavespeed_list)
+
+
+def crop_center(img, crop_size, scaler=2):
+    """
+    Parameters
+    ----------
+    img : (numpy / pytorch tensor) input image to crop
+    crop_size : (int) size of crop
+    boundary_condition : (string) choice of boundary condition, "periodic" or "absorbing"
+    scaler : scale factor
+
+    Returns
+    -------
+    crop center of img given size of crop, and scale factor
+    """
+
+    y, x = img.shape
+    startx = x // scaler - (crop_size // scaler)
+    starty = y // scaler - (crop_size // scaler)
+
+    return img[starty : starty + crop_size, startx : startx + crop_size]
+
+
+def initial_condition_gaussian(vel, mode, res_padded):
+    """
+    Parameters
+    ----------
+    vel : (numpy tensor) velocity profile
+    resolution : (int) resolution of actual area to propagate wave
+    optimization : (string) optimization technique; "parareal" or "none"
+    mode : (string) defines initial condition representation; "physical_components" or "energy_components"
+    res_padded : (int) resolution of padded area to propagate wave, we need a larger resolution in case of "parareal" and / or "absorbing"
+
+    Returns
+    -------
+    generates a Gaussian pulse to be used as an initial condition for our end-to-end model to advance waves
+    """
+
+    dx, width, center_x, center_y = 2.0 / 128.0, 7000, 0, 0
+    u0, ut0 = init_pulse_gaussian(width, res_padded, center_x, center_y)
+
+    if mode == "physical_components":
+        return u0, ut0
+    else:  # energy_components
+        u0, ut0 = torch.from_numpy(u0).unsqueeze(dim=0), torch.from_numpy(
+            ut0
+        ).unsqueeze(dim=0)
+        wx, wy, wtc = WaveEnergyComponentField_tensor(
+            u0, ut0, vel.unsqueeze(dim=0), dx=dx
+        )
+        return torch.stack([wx, wy, wtc], dim=1)
+
+
+def one_iteration_pseudo_spectral_tensor(
+    u_n_k, f_delta_x=2.0 / 128.0, f_delta_t=(2.0 / 128.0) / 20.0, delta_t_star=0.06
+):
+    """
+
+    Parameters
+    ----------
+    u_n_k : (pytorch tensor) wave representation as energy components
+    f_delta_x : (float) spatial step size / grid spacing (in x_1 and x_2 dimension)
+    f_delta_t : (float) temporal step size
+    delta_t_star : (float) time step a solver propagates a wave and solvers are compared
+
+    Returns
+    -------
+    propagates a wave for one time step delta_t_star using the pseudo-spectral method
+    """
+
+    u, u_t = WaveSol_from_EnergyComponent_tensor(
+        u_n_k[:, 0, :, :].clone(),
+        u_n_k[:, 1, :, :].clone(),
+        u_n_k[:, 2, :, :].clone(),
+        u_n_k[:, 3, :, :].clone(),
+        f_delta_x,
+        torch.sum(torch.sum(torch.sum(u_n_k[:, 0, :, :].clone()))),
+    )
+    vel = u_n_k[:, 3, :, :].clone()
+    u_prop, u_t_prop = pseudo_spectral_tensor(
+        u, u_t, vel, f_delta_x, f_delta_t, delta_t_star
+    )
+    u_x, u_y, u_t_c = WaveEnergyComponentField_tensor(
+        u_prop, u_t_prop, vel.unsqueeze(dim=1), f_delta_x
+    )
+    return torch.stack([u_x, u_y, u_t_c], dim=1)
---
+++ wave_component_function.py
@@ -0,0 +1,146 @@
+import numpy as np
+import torch
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def WaveEnergyComponentField_tensor(u, ut, c, dx):
+    """
+    Parameters
+    ----------
+    u : (pytorch tensor) physical wave component, displacement of wave
+    ut : (pytorch tensor) physical wave component derived by t, velocity of wave
+    c : (pytorch tensor) velocity profile dependent on x_1 and x_2
+    dx : (float) time step in both dimensions / grid spacing
+
+    Returns
+    -------
+    batchwise transformation to energy components of wave given parameters c and dx (tensor)
+    function to use when applying end-to-end model
+    """
+
+    wx = torch.zeros(u.shape)
+    wy = torch.zeros(u.shape)
+    wtc = torch.zeros(u.shape)
+    for b in range(u.shape[0]):
+        wx[b, :, :], wy[b, :, :] = torch.gradient(u[b, :, :], spacing=dx)
+        wtc[b, :, :] = torch.divide(ut[b, :, :], c[b, :, :])
+    return wx, wy, wtc
+
+
+def WaveSol_from_EnergyComponent_tensor(wx, wy, wtc, c, dx, sumv):
+    """
+    Parameters
+    ----------
+    wx : (pytorch tensor) energy wave component, u derived by x_1
+    wy : (pytorch tensor) energy wave component, u derived by x_2
+    wtc : (pytorch tensor) energy wave component, u derived by t and divided by c
+    c : (numpy tensor) velocity profile dependent on x_1 and x_2
+    dx : (float) time step in both dimensions / grid spacing
+    sumv : (float) sum of first energy wave component, i.e. wx
+
+    Returns
+    -------
+    compute wave solution components from energy component
+    """
+
+    def _grad2func_tensor(vx, vy, dx, sumv):
+        """
+        Parameters
+        ----------
+        vx : (numpy tensor) energy wave component, u derived by x_1
+        vy : (numpy tensor) energy wave component,  u derived by x_2
+        dx : (float) time step in both dimensions / grid spacing
+        sumv : (float) sum of first energy wave component, i.e. vx
+
+        Returns
+        -------
+        mapping gradient to functional value (numpy)
+        """
+
+        hatx = torch.fft.fft2(vx)
+        haty = torch.fft.fft2(vy)
+        ny, nx = vx.shape
+
+        xii = (
+            2
+            * torch.pi
+            / (dx * nx)
+            * torch.fft.fftshift(torch.linspace(-round(nx / 2), round(nx / 2 - 1), nx))
+        )
+        yii = (
+            2
+            * torch.pi
+            / (dx * ny)
+            * torch.fft.fftshift(torch.linspace(-round(ny / 2), round(ny / 2 - 1), ny))
+        )
+        yiyi, xixi = torch.meshgrid(xii, yii, indexing="xy")
+
+        radsq = torch.multiply(xixi, xixi) + torch.multiply(yiyi, yiyi)
+        radsq[0, 0] = 1
+        hatv = -1j * torch.divide(
+            (
+                torch.multiply(hatx.to(device), xixi.to(device))
+                + torch.multiply(haty.to(device), yiyi.to(device))
+            ),
+            radsq.to(device),
+        )
+        hatv[0, 0] = sumv
+
+        return torch.real(torch.fft.ifft2(hatv))
+
+    u = torch.zeros((wx.shape[0], wx.shape[-1], wx.shape[-1]))
+
+    for b in range(wx.shape[0]):
+        u[b, :, :] = _grad2func_tensor(wx[b, :, :], wy[b, :, :], dx, sumv)
+
+    ut = torch.multiply(wtc, c)
+
+    return u, ut
+
+
+def WaveEnergyComponentField_end_to_end(u, ut, c, dx):
+    """
+    Parameters
+    ----------
+    u : (numpy tensor) physical wave component, displacement of wave
+    ut : (numpy tensor) physical wave component derived by t, velocity of wave
+    c : (numpy tensor) velocity profile dependent on x_1 and x_2
+    dx : (float) time step in both dimensions / grid spacing
+
+    Returns
+    -------
+    batchwise transformation to energy components of wave given parameters c and dx (numpy)
+    function to use when applying end-to-end model
+    """
+
+    wx, wy = np.gradient(u, dx)
+    wtc = np.divide(ut, c)
+
+    return wx, wy, wtc
+
+
+def WaveEnergyField(u, ut, c, dx):
+    """
+
+    Parameters
+    ----------
+    u : (numpy tensor) physical wave component, displacement of wave
+    ut : (numpy tensor) physical wave component derived by t, velocity of wave
+    c : (numpy tensor) velocity profile dependent on x_1 and x_2
+    dx : (float) time step in both dimensions / grid spacing
+
+    Returns
+    -------
+    energy-semi norm of wave given parameters c and dx (numpy)
+    """
+
+    ux, uy = np.gradient(u, dx)
+    absux = np.abs(ux)
+    absuy = np.abs(uy)
+    absutc = np.divide(np.abs(ut), c)
+    w = (
+        np.multiply(absux, absux)
+        + np.multiply(absuy, absuy)
+        + np.multiply(absutc, absutc)
+    )
+
+    return w
